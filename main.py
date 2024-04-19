dc298089767a374d7fb12caeb7852a35ef9366b2
Vladimir <volosnikovvladimir69@gmail.com>
2024-04-19 17:10:52 +0500

    Added a data warehouse

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import asyncio
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from config_data import Config, load_config
 from keyboards import set_main_menu
-from handlers import standart_handlers
+from handlers import standart_handlers, query_handlers
 
 
 async def main() -> None:
@@ -15,13 +16,14 @@ async def main() -> None:
     config: Config = load_config()
     bot: Bot = Bot(token=config.tg_bot.token,
                    parse_mode='HTML')
-    dp: Dispatcher = Dispatcher()
+    storage: MemoryStorage = MemoryStorage()
+    dp: Dispatcher = Dispatcher(storage=storage)
 
     # вывод кнопки меню
     await set_main_menu(bot)
 
     # Регистрируем роутеры
-    dp.include_routers(*[standart_handlers.router])
+    dp.include_routers(*[standart_handlers.router, query_handlers.router])
 
     # Удалим необработанные апдейты
     await bot.delete_webhook(drop_pending_updates=True)
